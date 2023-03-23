5be32b3b963b816ea2e0c40dccb1111c5c5b3df2
afeiship <1290657123@qq.com>
2023-03-23 11:18:57 +0800

    fix: use latest generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const Generator = require('yeoman-generator');
+const Generator = require('@jswork/yeoman-generator');
 const globby = require('globby');
 const getp = require('@jswork/generator-prompts');
 const prompts = getp([
