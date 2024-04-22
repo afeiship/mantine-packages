568a0514327e73663008874088c06e9dd7ffcc10
afeiship <1290657123@qq.com>
2024-04-23 06:46:05 +0800

    feat: add checkbox for mantine

--- packages/mantine-components/public/src/app.tsx
+++ packages/mantine-components/public/src/app.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { LoadingOverlay } from '@/main';
+import { Checkbox } from '@/main';
 
 export default () => {
+  const [checked, setChecked] = React.useState(true);
+
+  // simulate a server response
+  setTimeout(() => {
+    console.log('server response received');
+    setChecked(false);
+  }, 1000);
+
   return (
-    <div
-      className="relative p-5 border w-4/5 mx-auto mt-10 bg-gray-500 rounded-md hover:bg-gray-200"
-      style={{ height: 2000 }}>
-      <LoadingOverlay className="debug-red" customCentered visible={true} />
-      <div className="content">
-        <p>道可道，非常道；名可名，非常名。</p>
-        <p>无名，天地之始，有名，万物之母。</p>
-        <p>故常无欲，以观其妙，常有欲，以观其徼。</p>
-        <p>此两者，同出而异名，同谓之玄，玄之又玄，众妙之门。</p>
-      </div>
+    <div className="relative border w-4/5 mx-auto mt-10">
+      <Checkbox value={checked} onChange={setChecked} label="I agree to the terms and conditions" />
+      <pre className="p-5 bg-gray-100">{JSON.stringify({ checked }, null, 2)}</pre>
     </div>
   );
 };
--- packages/mantine-components/src/main.ts
+++ packages/mantine-components/src/main.ts
@@ -1,3 +1,4 @@
 import LoadingOverlay from './components/loading-overlay';
+import Checkbox from './components/checkbox';
 
-export { LoadingOverlay };
+export { LoadingOverlay, Checkbox };
