d0657be86e8b99fd8b63980783ff368435ae15e4
afeiship <1290657123@qq.com>
2024-04-23 06:23:19 +0800

    feat: add default props for overlay

--- packages/mantine-components/src/components/loading-overlay.tsx
+++ packages/mantine-components/src/components/loading-overlay.tsx
@@ -12,12 +12,16 @@ interface LoadingOverlayExtendProps extends LoadingOverlayProps {
 }
 
 function LoadingOverlay(props: LoadingOverlayExtendProps) {
-  const { theme, customCentered = false, ...rest } = props;
+  const { theme, customCentered, ...rest } = props;
   return (
     <MantineProvider theme={theme}>
       <LoadingOverlayComponent data-custom-centered={customCentered} {...rest} />
     </MantineProvider>
   );
 }
+
+LoadingOverlay.defaultProps = {
+  customCentered: false
+};
 
 export default LoadingOverlay;
