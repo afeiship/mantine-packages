f3a2a9bc9ab4c3e7ee3873ce4625916a1a112aea
afeiship <1290657123@qq.com>
2024-04-23 21:33:26 +0800

    feat: add menu props

--- packages/mantine-components/src/main.ts
+++ packages/mantine-components/src/main.ts
@@ -1,7 +1,8 @@
 import CheckboxGroup from './components/checkbox-group';
 import LoadingOverlay from './components/loading-overlay';
+import Menu from './components/menu';
 import RadioGroup from './components/radio-group';
 import SegmentedControl from '@/components/segmented-control';
 import Selection from './components/selection';
 
-export { CheckboxGroup, LoadingOverlay, RadioGroup, SegmentedControl, Selection };
+export { CheckboxGroup, LoadingOverlay, Menu, RadioGroup, SegmentedControl, Selection };
