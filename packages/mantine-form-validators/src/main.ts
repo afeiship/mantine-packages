f9e5cde25d1fd26bd81c0c664a6d8d21be282e53
afeiship <1290657123@qq.com>
2024-08-28 07:03:09 +0800

    fix: rm nx.

--- packages/mantine-form-validators/src/main.ts
+++ packages/mantine-form-validators/src/main.ts
@@ -1,8 +1,7 @@
 import MantineFormValidators, { MantineFormValidatorsProps } from './components';
 import defineRule, { Rule } from './components/define-rule';
-import useRule from './components/use-rule';
 
 export default MantineFormValidators;
 export type { MantineFormValidatorsProps, Rule };
 
-export { defineRule, useRule };
+export { defineRule };
